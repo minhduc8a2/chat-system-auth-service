9d4817ff2b3a2a0c3b1c6a95caaef48f86dfda30
Duke Lee <minhduc8a2.1@gmail.com>
2025-04-12 18:24:04 +0700

    add redis to cache user by username

--- src/main/java/com/ducle/authservice/service/CustomUserDetailsService.java
+++ src/main/java/com/ducle/authservice/service/CustomUserDetailsService.java
@@ -3,6 +3,7 @@ package com.ducle.authservice.service;
 import java.util.Collection;
 import java.util.Collections;
 
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -22,6 +23,7 @@ public class CustomUserDetailsService implements UserDetailsService {
     private final UserRepository userRepository;
 
     @Override
+    @Cacheable(value = "users", key = "#username")
     public CustomUserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepository.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
