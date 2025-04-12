871834c35f5f794f389d317097915cb4906673bc
Duke Lee <minhduc8a2.1@gmail.com>
2025-04-12 15:27:40 +0700

    implement AuthController

--- src/main/java/com/ducle/authservice/controller/AuthController.java
+++ src/main/java/com/ducle/authservice/controller/AuthController.java
@@ -4,22 +4,31 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ducle.authservice.model.dto.AuthResponse;
+import com.ducle.authservice.model.dto.LoginRequest;
+import com.ducle.authservice.model.dto.RegisterRequest;
+import com.ducle.authservice.service.AuthService;
+
+import lombok.RequiredArgsConstructor;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-
 @RestController
 @RequestMapping("${api.auth.url}")
+@RequiredArgsConstructor
 public class AuthController {
 
+    private final AuthService authService;
+
     @PostMapping("/login")
-    public ResponseEntity<AuthResponse> login(@RequestBody String entity) {
-        
-        
-        return entity;
+    public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest loginRequest) {
+        return ResponseEntity.ok(authService.login(loginRequest));
     }
-    
-    
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthResponse> register(@RequestBody RegisterRequest registerRequest) {
+        return ResponseEntity.ok(authService.register(registerRequest));
+    }
+
 }
