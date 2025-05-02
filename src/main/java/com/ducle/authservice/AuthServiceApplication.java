91182a640a67e74c58eccddf453e62bbb4ec0ebe
Duke Lee <minhduc8a2.1@gmail.com>
2025-05-02 12:19:58 +0700

    config on cloud

--- src/main/java/com/ducle/authservice/AuthServiceApplication.java
+++ src/main/java/com/ducle/authservice/AuthServiceApplication.java
@@ -2,12 +2,10 @@ package com.ducle.authservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
-@EnableCaching
 public class AuthServiceApplication {
 
 	public static void main(String[] args) {
